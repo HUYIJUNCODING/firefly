75ed79f6134c9e206ae4efb86000eba9c4303c9f
ldy <stone_dev@163.com>
2020-12-27 22:48:20 +0800

    add and edit client

---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        {{ name }}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+    setup() {
+        const name = ref('hello-firefly');
+
+        return {
+            name
+        };
+    }
+});
+</script>
